e8f53b62b43c4282858eb5acad78fb13de28aade
tegra <tegra@TEGRABALONGA>
2023-09-15 02:55:31 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using JR_Extreme_Prototype.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace JR_Extreme_Prototype.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JR_Extreme_Prototype.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
